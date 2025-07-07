7917298242c29fa8d98a040d8d8b139f01e2be0c
Nikita Sachan <134432831+Nikita2004170@users.noreply.github.com>
2025-07-08 00:15:06 +0530

    Create api_constant.dart

---
+++ lib/common/utils/api_constant.dart
@@ -0,0 +1,37 @@
+String? accessToken;
+const String BASE_URL = "https://api.test.starastrogpt.com/";
+// "http://ec2-13-233-168-231.ap-south-1.compute.amazonaws.com/";
+const String geoCoder = "https://geocoder.test.starastrogpt.com/";
+const String dashboard = "https://dashboard.test.starastrogpt.com/";
+
+const String CTypeFormData = "multipart/form-data";
+const String CTypeJson = "application/json";
+const String CTypeFormDataEncoded = "application/x-www-form-urlencoded";
+
+const String apiLogin = "auth/signin";
+const String apiGoogle = "auth/google";
+const String apiCreateUser = "auth/signup";
+const String apiVerifyOtp = "auth/verify-otp";
+const String apiUpdateMobileNumber = "auth/update-mobile";
+const String apiForgotPassword = "auth/forgot-password";
+const String apiResetPassword = "auth/reset-password";
+const String apiAccessToken = "auth/issue-access-token";
+const String apiChangePassword = "auth/change-password";
+
+const String apiUser = "user";
+
+const String apiHoroscope = "horoscope";
+
+const String searchPlaces = "${geoCoder}places/search";
+
+const String plans = "plans";
+const String paymentOrder = "payment/stripe/order";
+const String razorpayOrder = "payment/razorpay/order";
+const String paymentVerify = "payment/verify/";
+const String notification = "user/notification";
+
+//RazorPay
+const String razorPayKey = "rzp_test_Vveqt66gtSPZ3B";
+const String razorPayKeySecret = "kBReIMveY7TxhEyYEH28pSAv";
+
+
